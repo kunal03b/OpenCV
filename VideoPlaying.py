cd23cd2d4d62eec54452356aecd3be04fb38babd
KUNAL SHARMA <kunal03b@gmail.com>
2022-11-08 20:47:36 +0530

    initial_commit

---
+++ VideoPlaying.py
@@ -0,0 +1,16 @@
+# Video Operation using OpenCV
+
+import cv2
+
+vid1 = input("Enter the path of the video")
+cap = cv2.VideoCapture(vid1)
+print("cap",cap)
+
+while(True):
+    ret, frame = cap.read()
+    frame = cv2.resize(frame(500,500))
+    cv2.imshow("frame",frame)
+    cv2.waitKey(25)
+
+cap.release()
+cv2.destroyAllWindows()
