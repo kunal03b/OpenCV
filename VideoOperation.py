de9aebe91c8fe500f5b7c442d2068eebc4f54bc3
KUNAL SHARMA <kunal03b@gmail.com>
2022-11-08 21:43:24 +0530

    Initial

--- VideoOperation.py
+++ VideoOperation.py
@@ -9,8 +9,11 @@ print("cap",cap)
 while(True):
     ret, frame = cap.read()
     frame = cv2.resize(frame(500,500))
+    # To convert Video to gray scale
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow("frame",frame)
-    cv2.waitKey(25)
+    cv2.imshow("GrayScale Image",gray)
+    cv2.waitKey(25)   #here 25 is the playback speed i.e. the less will be the value the more will be the speed of video
     # k = cv2.waitKey(25)
     # if k == ord('e'):
     #     break
