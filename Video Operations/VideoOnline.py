f8a147842fec9af4adcce55c298b1e3176ffbd04
KUNAL SHARMA <kunal03b@gmail.com>
2022-11-09 09:34:13 +0530

    initialCommit

---
+++ Video Operations/VideoOnline.py
@@ -0,0 +1,29 @@
+# Program to capture video from Youtube
+
+import cv2
+import pafy
+
+url = input("Enter the URL of video")
+data = pafy.new(url)
+data = data.getbest(preftype="mp4")
+
+cap = cv2.VideoCapture(0)
+cap.open(data.url)
+print("Loading...",cap.isOpened)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret == True:
+        # commented because frame already taken while saving video in above code
+        frame = cv2.resize(frame(500,500))
+        cv2.imshow('frame', frame)
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        cv2.imshow('gray', gray)
+
+        # To exit the video capture. Here in waitkey 1 is used to for dynamic capture i.e. video and 0 is used for image capture
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+
+cv2.destroyAllWindows()
