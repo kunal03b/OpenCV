1e893806d947dfb14e3fb97fe2422dcc2fe919fe
KUNAL SHARMA <kunal03b@gmail.com>
2022-11-09 09:37:01 +0530

    Save Function Added

--- Video Operations/VideoOnline.py
+++ Video Operations/VideoOnline.py
@@ -6,10 +6,14 @@ import pafy
 url = input("Enter the URL of video")
 data = pafy.new(url)
 data = data.getbest(preftype="mp4")
+path= input("Enter the path where you want to save the video")
 
 cap = cv2.VideoCapture(0)
 cap.open(data.url)
 print("Loading...",cap.isOpened)
+
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+output = cv2.VideoWriter(path,fourcc,50.0,(700,700)) 
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -19,11 +23,11 @@ while cap.isOpened():
         cv2.imshow('frame', frame)
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         cv2.imshow('gray', gray)
-
+        output.write(frame)
         # To exit the video capture. Here in waitkey 1 is used to for dynamic capture i.e. video and 0 is used for image capture
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 cap.release()
-
+output.release()
 cv2.destroyAllWindows()
