99ec0d6a558222b3670850920130873183a83645
KUNAL SHARMA <kunal03b@gmail.com>
2022-11-13 22:56:01 +0530

    updated

--- Image processing/ColorDetection.py
+++ Image processing/ColorDetection.py
@@ -14,7 +14,35 @@ cv2.createTrackbar("Lower_H","Color Adjustment",0,255,cross)
 cv2.createTrackbar("Lower_S","Color Adjustment",0,255,cross)
 cv2.createTrackbar("Lower_V","Color Adjustment",0,255,cross)
 
-cv2.createTrackbar("Upper_H","Color Adjustment",0,255,cross)
-cv2.createTrackbar("Upper_S","Color Adjustment",0,255,cross)
-cv2.createTrackbar("Upper_V","Color Adjustment",0,255,cross)
+cv2.createTrackbar("Upper_H","Color Adjustment",255,255,cross)
+cv2.createTrackbar("Upper_S","Color Adjustment",255,255,cross)
+cv2.createTrackbar("Upper_V","Color Adjustment",255,255,cross)
 
+# Binding the Trackbar
+while True:
+    hsv  = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+    l_h = cv2.getTrackbarPos("Lower_H","Color Adjustment")
+    l_s = cv2.getTrackbarPos("Lower_S","Color Adjustment")
+    l_v = cv2.getTrackbarPos("Lower_V","Color Adjustment")
+
+    u_h = cv2.getTrackbarPos("Upper_H","Color Adjustment")
+    u_s = cv2.getTrackbarPos("Upper_S","Color Adjustment")
+    u_v = cv2.getTrackbarPos("Upper_V","Color Adjustment")
+
+    # Passing the lower and upper bound values
+    lower_BV = np.array([l_h,l_s,l_v])
+    upper_BV = np.array([u_h,u_s,u_v])
+
+    # getting the masked image 
+    mask = cv2.inRange(hsv,lower_BV,upper_BV)
+
+    # filter mask with image
+    res = cv2.bitwise_and(frame,frame,mask=mask)   #color will be reflected by mask variable as upper and lower bound values are stored in mask
+    
+    cv2.imshow("frame",frame)
+    cv2.imshow("mask",mask)
+    cv2.imshow("res",res)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
