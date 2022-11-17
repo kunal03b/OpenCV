2506b9c40a9a55385e28fc7fbb578e04146df8d8
KUNAL SHARMA <kunal03b@gmail.com>
2022-11-17 22:34:29 +0530

    upated

--- Image processing/cannyEdge.py
+++ Image processing/cannyEdge.py
@@ -9,11 +9,23 @@ img = cv2.imread(image)
 img = cv2.resize(img,(400,600))
 Gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-canny_img = cv2.Canny(Gray_img,60,150)
+"""canny_img = cv2.Canny(Gray_img,60,150)
 
 cv2.imshow("Original Image",img)
 cv2.imshow("Gray Image",Gray_img)
 cv2.imshow("Canny Image",canny_img)
+"""
+# Applying Canny Edge Detection using Trackbar
+def cross(x):
+    pass
+
+cv2.namedWindow("Canny Image")
+cv2.createTrackbar("Threshold Value","Canny Image",0,255,cross)
+
+while True:
+    pos = cv2.getTrackbarPos("Threshold Value","Canny Image")
+    print(pos)
+    output = cv2.Canny(Gray_img,pos,255)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
